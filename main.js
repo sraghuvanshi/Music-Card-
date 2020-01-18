fa7fcf7bf363436fb54496c8ca0f7347c15ece87
rsaksham <sakshamraghuvanshi1@gmail.com>
2020-01-18 14:36:58 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var audio = new Audio(
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/Lecrae_-_Anomaly_(Lyric_Video).mp3"
+);
+audio.volume = 0.1;
+audio.autoplay = true;
+
+$(".trigger").click(function() {
+  if (audio.paused == false) {
+    audio.pause();
+    $(".fa-play").show();
+    $(".fa-pause").hide();
+    $(".music-card").addClass("playing");
+  } else {
+    audio.play();
+    $(".fa-pause").show();
+    $(".fa-play").hide();
+    $(".music-card").removeClass("playing");
+  }
+});
